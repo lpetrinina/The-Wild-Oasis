b1e4be5e7581871082d9688ddc37b9c34d17ed9d
Luiza Petrinina <petrinina95@gmail.com>
2025-10-09 13:59:55 +0300

    Create a global styled component
    Includes: reset CSS, main app colors, basic styles

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  background-color: bisque;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Work!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1> Work!</H1>
+        <Button onClick={() => alert("Check in!")}>Check in</Button>
+        <Button onClick={() => alert("Check out!")}>Check out</Button>
+
+        <Input type='number' placeholder='Number of guests' />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
