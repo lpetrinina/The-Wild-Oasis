2bf480d7df703a1cd50e135950ba0c8101094e14
Luiza Petrinina <petrinina95@gmail.com>
2025-10-10 16:09:25 +0300

    Build the App Layout

---
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router";
+import styled from "styled-components";
+
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 26rem 1fr;
+
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
