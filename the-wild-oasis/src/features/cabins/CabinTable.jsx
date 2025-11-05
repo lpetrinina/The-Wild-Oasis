6cf5022d4db111f33708f0e3b0b42d68df0de57b
Luiza Petrinina <petrinina95@gmail.com>
2025-11-05 21:34:07 +0200

    Build a reusable table

--- the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 import { useCabins } from "./useCabins";
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import Table from "../../ui/Table";
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
+// const Table = styled.div`
+//   border: 1px solid var(--color-grey-200);
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+//   font-size: 1.4rem;
+//   background-color: var(--color-grey-0);
+//   border-radius: 7px;
+//   overflow: hidden;
+// `;
 
 const TableHeader = styled.header`
   display: grid;
@@ -37,15 +37,15 @@ function CabinTable() {
   }
 
   return (
-    <Table role='table'>
-      <TableHeader role='row'>
+    <Table $columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+      <Table.Header role='row'>
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
+      </Table.Header>
 
       {cabins.map((cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
