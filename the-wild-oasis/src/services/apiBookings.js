3c21578fe2ed07b0b5d12d43578b81851fdf8186
Luiza Petrinina <petrinina95@gmail.com>
2025-11-17 18:22:27 +0200

    Fix booking api
    Get only the necessary data

--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -3,12 +3,14 @@ import supabase from "./supabase";
 
 export async function getBookings() {
     const { data, error } = await supabase
-        .from('bookings')
-        .select('*');
+        .from("bookings")
+        .select(
+            "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, status, cabins(name), guests(fullName, email)"
+        );
 
     if (error) {
         console.error(error);
-        throw new Error('Bookings could not be loaded!');
+        throw new Error("Bookings could not be loaded!");
     }
 
     return data;
