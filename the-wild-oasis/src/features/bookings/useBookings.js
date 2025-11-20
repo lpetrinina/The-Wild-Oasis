25b558667a40fba6bb9dcca0d52fc23b28873b61
Luiza Petrinina <petrinina95@gmail.com>
2025-11-20 15:53:39 +0200

    Prefetch booking data
    Prefetch previous and next booking pages

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,26 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
 
-    // * Filter
+    // * FILTER *
     const filterValue = searchParams.get("status");
     const filter =
         !filterValue || filterValue === "all"
             ? null
             : { field: 'status', value: filterValue };
 
-    // ** Sort
+    // * SORT *
     const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
     const [field, direction] = sortByRaw.split('-');
     const sortBy = { field, direction };
 
-    // *** Pagination
+    // * PAGINATION *
     const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
-
+    //  * QUERY *
     const {
         isPending,
         data: { data: bookings, count } = {},
@@ -29,6 +31,25 @@ export function useBookings() {
         queryKey: ["bookings", filter, sortBy, page], //A unique key that helps ReactQuery know if it needs to fetch data (the key doesn't exist in cache) or not.
         queryFn: () => getBookings({ filter, sortBy, page })
     });
+
+
+    // * PREFETCHING *
+    const pageCount = Math.ceil(count / PAGE_SIZE);
+
+    if (page < pageCount) {
+
+        queryClient.prefetchQuery({
+            queryKey: ["bookings", filter, sortBy, page + 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page + 1 })
+        })
+    }
+
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ['bookings', filter, sortBy, page - 1],
+            queryFn: () => getBookings({ filter, sortBy, page: page - 1 })
+        })
+    }
 
 
     return { isPending, bookings, count };
