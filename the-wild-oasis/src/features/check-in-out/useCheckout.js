1aa0dcb38c504a581d8227689287bd789f51c729
Luiza Petrinina <petrinina95@gmail.com>
2025-11-25 18:36:16 +0200

    Create a useCheckout custom hook

---
+++ the-wild-oasis/src/features/check-in-out/useCheckout.js
@@ -0,0 +1,29 @@
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { updateBooking } from "../../services/apiBookings";
+
+export function useCheckout() {
+    const queryClient = useQueryClient();
+    // const navigate = useNavigate();
+
+    const { mutate: checkOut, isPending: isChekingOut } = useMutation({
+
+        mutationFn: (bookingId) => updateBooking(bookingId, {
+            status: 'checked-out',
+        }),
+
+        onSuccess: (data, bookingId) => {
+            toast.success(`Booking #${data.id} successfully checked out!`);
+            queryClient.invalidateQueries({ queryKey: ['booking', bookingId] });
+
+            // navigate('/');
+        },
+
+        onError: () => toast.error('There was an error while checking out ')
+    });
+
+
+    return { checkOut, isChekingOut };
+}
