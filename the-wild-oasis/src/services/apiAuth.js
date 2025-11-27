5edd7defd70097178130244639bd4eef30ccea4c
Luiza Petrinina <petrinina95@gmail.com>
2025-11-27 18:58:46 +0200

    Add getCurrentUser to apiAuth

--- the-wild-oasis/src/services/apiAuth.js
+++ the-wild-oasis/src/services/apiAuth.js
@@ -13,3 +13,23 @@ export async function login({ email, password }) {
     console.log(data)
     return data;
 }
+
+export async function getCurrentUser() {
+
+    const { data: sessionData } = await supabase.auth.getSession();
+
+    if (!sessionData.session) {
+        return null;
+    }
+
+    const { data, error } = await supabase.auth.getUser();
+
+    console.log(data);
+
+    if (error) {
+        throw new Error(error.message)
+    };
+
+
+    return data?.user;
+}
