591faf2074a442028fb29c543edf17976daffebc
Luiza Petrinina <petrinina95@gmail.com>
2025-11-28 16:51:59 +0200

    Implement logout

---
+++ the-wild-oasis/src/features/authentication/Logout.jsx
@@ -0,0 +1,18 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+import ButtonIcon from "../../ui/ButtonIcon";
+
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { isPending, logout } = useLogout();
+
+  return (
+    <ButtonIcon disabled={isPending} onClick={() => logout()}>
+      {!isPending ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
