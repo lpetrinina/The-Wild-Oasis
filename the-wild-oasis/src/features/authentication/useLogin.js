c9af41d00c538575168321320978b54581cfca9a
Luiza Petrinina <petrinina95@gmail.com>
2025-11-28 18:11:17 +0200

    Optimize useLogin hook
    onSuccess  the cached data for the user will be immediately updated without making a new network request

--- the-wild-oasis/src/features/authentication/useLogin.js
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,17 +1,19 @@
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
 
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const { isPending, mutate: login } = useMutation({
         mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-        onSuccess: (user) => {
+        onSuccess: (data) => {
+            queryClient.setQueryData(['user'], data.user); //update the existing query with the new data immediately, without wasting a network call for data we already have
             navigate('/dashboard', { replace: true });
         },
 
