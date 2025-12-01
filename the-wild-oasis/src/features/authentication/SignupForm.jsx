aa22754021e7697e25387290b2b7fd18fa1f47bc
Luiza Petrinina <petrinina95@gmail.com>
2025-12-01 18:00:14 +0200

    Implement the sign up functionality

--- the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -4,15 +4,22 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isPending } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  const onSubmit = function (data) {
-    console.log(data);
+  const onSubmit = function ({ fullName, email, password }) {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
   };
 
   return (
@@ -21,6 +28,7 @@ function SignupForm() {
         <Input
           type='text'
           id='fullName'
+          disabled={isPending}
           {...register("fullName", { required: "This field is required!" })}
         />
       </FormRow>
@@ -29,6 +37,7 @@ function SignupForm() {
         <Input
           type='email'
           id='email'
+          disabled={isPending}
           {...register("email", {
             required: "This field is required!",
             pattern: {
@@ -46,6 +55,7 @@ function SignupForm() {
         <Input
           type='password'
           id='password'
+          disabled={isPending}
           {...register("password", {
             required: "This field is required!",
             minLength: {
@@ -60,6 +70,7 @@ function SignupForm() {
         <Input
           type='password'
           id='passwordConfirm'
+          disabled={isPending}
           {...register("passwordConfirm", {
             required: "This field is required!",
             validate: (value) =>
@@ -70,10 +81,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button $variation='secondary' type='reset'>
+        <Button $variation='secondary' type='reset' disabled={isPending}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isPending}>Create new user</Button>
       </FormRow>
     </Form>
   );
