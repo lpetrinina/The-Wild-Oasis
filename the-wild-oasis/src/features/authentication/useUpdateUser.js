6ed6c3c78c0b4016fe1578e029b391f9120105b0
Luiza Petrinina <petrinina95@gmail.com>
2025-12-02 18:58:41 +0200

    Create useUpdateUser hook

---
+++ the-wild-oasis/src/features/authentication/useUpdateUser.js
@@ -0,0 +1,23 @@
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+    const queryClient = useQueryClient();
+
+    const { mutate: updateUser, isPending: isUpdating } = useMutation({
+        mutationFn: ({ password, fullName, avatar }) =>
+            updateCurrentUser({ password, fullName, avatar }),
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['user'] });
+
+            toast.success('User account is successfully updated!');
+        },
+
+        onError: (err) => toast.error(err.message),
+    });
+
+    return { updateUser, isUpdating }
+}
