344b2944d038f0bc56cef372442a033b374985fc
Luiza Petrinina <petrinina95@gmail.com>
2025-12-02 17:19:25 +0200

    Build the app header

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("luiza@example.com");
-  const [password, setPassword] = useState("pass1114");
+  const [email, setEmail] = useState("test@gmail.com");
+  const [password, setPassword] = useState("12345678");
   const { isPending, login } = useLogin();
 
   function handleSubmit(e) {
