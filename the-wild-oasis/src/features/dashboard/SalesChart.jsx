18fb75c755f27e5624f64fc1c2e9db3f8580afac
Luiza Petrinina <petrinina95@gmail.com>
2025-12-05 17:58:09 +0200

    Build the Sales Chart component

--- the-wild-oasis/src/features/dashboard/SalesChart.jsx
+++ the-wild-oasis/src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,19 @@
 import styled from "styled-components";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
+
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -57,3 +71,64 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+  });
+
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
+        extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
+        text: "#e5e7eb",
+        background: "#18212f",
+      }
+    : {
+        totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
+        extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
+        text: "#374151",
+        background: "#fff",
+      };
+
+  return (
+    <StyledSalesChart>
+      <Heading as='h2'>Sales</Heading>
+
+      <ResponsiveContainer height={300} width='100%'>
+        <AreaChart data={fakeData}>
+          <XAxis
+            dataKey='label'
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <YAxis
+            unit='$'
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+
+          <CartesianGrid strokeDasharray='4' />
+          <Tooltip contentStyle={{ background: colors.background }} />
+
+          <Area
+            dataKey='totalSales'
+            type='monotone'
+            stroke={colors.totalSales.stroke}
+            fill={colors.totalSales.fill}
+            strokeWidth={2}
+            name='Total sales'
+            unit='$'
+          />
+          <Area
+            dataKey='extrasSales'
+            type='monotone'
+            stroke={colors.extrasSales.stroke}
+            fill={colors.extrasSales.fill}
+            strokeWidth={2}
+            name='Extras sales'
+            unit='$'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </StyledSalesChart>
+  );
+}
+
+export default SalesChart;
