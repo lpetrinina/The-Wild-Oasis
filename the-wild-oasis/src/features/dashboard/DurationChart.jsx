bca5e028f20a777c6025890bb86277fa28263940
Luiza Petrinina <petrinina95@gmail.com>
2025-12-05 18:52:46 +0200

    Build the Duration chart component

--- the-wild-oasis/src/features/dashboard/DurationChart.jsx
+++ the-wild-oasis/src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,14 @@
 import styled from "styled-components";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+import Heading from "../../ui/Heading";
 
 const ChartBox = styled.div`
   /* Box */
@@ -26,22 +36,22 @@ const startDataLight = [
   },
   {
     duration: "2 nights",
-    value: 0,
+    value: 8,
     color: "#f97316",
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 6,
     color: "#eab308",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 10,
     color: "#84cc16",
   },
   {
     duration: "6-7 nights",
-    value: 0,
+    value: 2,
     color: "#22c55e",
   },
   {
@@ -51,7 +61,7 @@ const startDataLight = [
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 1,
     color: "#3b82f6",
   },
   {
@@ -116,6 +126,7 @@ function prepareData(startData, stays) {
   const data = stays
     .reduce((arr, cur) => {
       const num = cur.numNights;
+
       if (num === 1) return incArrayValue(arr, "1 night");
       if (num === 2) return incArrayValue(arr, "2 nights");
       if (num === 3) return incArrayValue(arr, "3 nights");
@@ -124,9 +135,54 @@ function prepareData(startData, stays) {
       if (num >= 8 && num <= 14) return incArrayValue(arr, "8-14 nights");
       if (num >= 15 && num <= 21) return incArrayValue(arr, "15-21 nights");
       if (num >= 21) return incArrayValue(arr, "21+ nights");
+
       return arr;
     }, startData)
     .filter((obj) => obj.value > 0);
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+  return (
+    <ChartBox>
+      <Heading as='h2'>Stay duration summary</Heading>
+
+      <ResponsiveContainer width='100%' height={240}>
+        <PieChart>
+          <Pie
+            data={startDataLight}
+            nameKey='duration'
+            dataKey='value'
+            innerRadius={85}
+            outerRadius={110}
+            cx='40%'
+            cy='50%'
+            paddingAngle={3}
+          >
+            {startDataLight.map((entry) => (
+              <Cell
+                key={entry.duration}
+                fill={entry.color}
+                stroke={entry.color}
+              />
+            ))}
+          </Pie>
+
+          <Tooltip />
+
+          <Legend
+            verticalAlign='middle'
+            align='right'
+            width='30%'
+            layout='vertical'
+            iconSize={15}
+            iconType='circle'
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
