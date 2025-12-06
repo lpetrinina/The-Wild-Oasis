171e989e298b281005311df976218593d229bf37
Luiza Petrinina <petrinina95@gmail.com>
2025-12-06 15:19:03 +0200

    Add check-in and checkout button to each Today Item

--- the-wild-oasis/src/features/check-in-out/CheckoutButton.jsx
+++ the-wild-oasis/src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,17 @@
 import Button from "../../ui/Button";
 
+import { useCheckout } from "./useCheckout";
+
 function CheckoutButton({ bookingId }) {
+  const { checkOut, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      $variation='primary'
+      $size='small'
+      disabled={isCheckingOut}
+      onClick={() => checkOut(bookingId)}
+    >
       Check out
     </Button>
   );
